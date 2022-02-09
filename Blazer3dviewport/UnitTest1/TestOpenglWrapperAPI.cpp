5a959b558f64a537b1b3d93eb1278fb044235e22
LowFire <LowFire3@yahoo.com>
2022-02-09 14:02:28 -0700

    Implemeted tests. Added drawio diagram.

--- Blazer3dviewport/UnitTest1/TestOpenglWrapperAPI.cpp
+++ Blazer3dviewport/UnitTest1/TestOpenglWrapperAPI.cpp
@@ -51,53 +51,137 @@ namespace TestOpenglWrapperAPI
 			auto default_construtor = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects);
 			Assert::AreEqual(0, default_construtor->size()); //Check to ensure the there are no VAOs created.
 
-
 			/*
 			* Test non-default constructor that takes in the number of VAOs to create.
 			*/
 			auto sized_constructor = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects(5));
 			Assert::AreEqual(5, sized_constructor->size()); //Check if the correct number of objects are constructed;
-			auto vertex_arrays = sized_construtor->getAllVertexArrays(); //Returns the vector of shared pointers to bVertexArrays
+			auto vertex_arrays = sized_constructor->getAllVertexArrays(); //Returns the vector of weak pointers to bVertexArrays
 
 			//Test to see if all objects are valid
 			for each (auto v in vertex_arrays)
 			{
-				Assert::IsNotNull(v);
-				vertex_arrays.at(i)->bind();
-				Assert::IsTrue(glIsVertexArray(v->getOpenglName()));
+				auto ptr = v.lock();
+				Assert::IsTrue(ptr!=nullptr);
+				ptr->bind();
+				Assert::IsTrue(glIsVertexArray(ptr->getOpenglName()));
 			}
 
 			/*
-			* Test non-default constructor that takes in the number of VAOs and an array of names to give each VAO
+			* Test non-default constructor that takes in a list of labels.
 			*/
-			std::array<const char*, 3> names = { "Cube", "Pyramid", "Sphere" };
-			auto named_constructor = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects(3, names));
+			auto named_constructor = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects{ "Cube", "Pyramid", "Sphere" });
 			Assert::AreEqual(3, named_constructor->size());
 			vertex_arrays = named_constructor->getAllVertexArrays();
 
 			//Test to make sure each vertex array is constructed properly.
-			for(int i = 0; i < 3; i++)
+			for each (auto v in vertex_arrays)
 			{
-				Assert::IsNotNull(vertex_arrays.at(i));
-				vertex_arrays.at(i)->bind();
-				Assert::IsTrue(glIsVertexArray(vertex_arrays.at(i)->getOpenglName()));
-				Assert::AreEqual(vertex_arrays.at(i)->getLabel(), names[i]);
+				auto ptr = v.lock();
+				Assert::IsTrue(ptr!=nullptr);
+				ptr->bind();
+				Assert::IsTrue(glIsVertexArray(ptr->getOpenglName()));
+			}
+
+			//Test to see if the labels have been initialized.
+			auto ptr = vertex_arrays.at(0).lock();
+			GLchar* cube_label = new GLchar[4];
+			Assert::AreEqual("Cube", ptr->label.c_str());
+			glGetObjectLabel(GL_VERTEX_ARRAY,
+				ptr->opengl_name,
+				4,
+				NULL,
+				cube_label);
+			Assert::AreEqual(ptr->label.c_str(), cube_label);
+			delete[] cube_label;
+
+			ptr = vertex_arrays.at(1).lock();
+			GLchar* pyramid_label = new GLchar[7];
+			Assert::AreEqual("Cube", ptr->label.c_str());
+			glGetObjectLabel(GL_VERTEX_ARRAY,
+				ptr->opengl_name,
+				7,
+				NULL,
+				cube_label);
+			Assert::AreEqual(ptr->label.c_str(), pyramid_label);
+			delete[] pyramid_label;
+
+			ptr = vertex_arrays.at(2).lock();
+			GLchar* sphere_label = new GLchar[6];
+			Assert::AreEqual("Cube", ptr->label.c_str());
+			glGetObjectLabel(GL_VERTEX_ARRAY,
+				ptr->opengl_name,
+				7,
+				NULL,
+				cube_label);
+			Assert::AreEqual(ptr->label.c_str(), pyramid_label);
+			delete[] sphere_label;
+
+
+			/*
+			* Test copy construction
+			*/
+			auto object1 = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects(3));
+			auto object2 = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects(*object1));
+			Assert::AreEqual(object1->size(), object2->size());
+
+			auto obj1_references = object1->getAllVertexArrays();
+			auto obj2_references = object2->getAllVertexArrays();
+			auto it1 = obj1_references.begin();
+			auto it2 = obj2_references.begin();
+			
+			//Check to see if the two objects have references to the same VAOs
+			for (; it1 != obj1_references.end() && it2 != obj2_references.end();
+				it1++, it2++)
+			{
+				auto ptr1 = it1->lock();
+				auto ptr2 = it2->lock();
+				Assert::AreSame(ptr1, ptr2);
 			}
 		}
 
-		TEST_METHOD(TestDestruction)
+		TEST_METHOD(TestCleanup)
 		{
+			/*
+			* Test basic cleanup.
+			*/
 			auto test_object = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects(3));
 			auto array_references = test_object->getAllVertexArrays();
 			test_object->~bVertexArrayObjects(); //Call the destructor
 			
 			for each (auto v in array_references)
 			{
-				Assert::IsNull(v);
-				Assert::IsFalse(glIsVertexArray(v->))
+				Assert::IsTrue(v.expired());
 			}
 
+			/*
+			* Test cleanup of two objects that have both shared references and unique ones.
+			*/
+			auto obj1 = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects(3));
+			auto obj2 = std::make_unique<bVertexArrayObjects>(new bVertexArrayObjects(*obj1));
+			obj1->createVertexArrays(1);
+			obj2->createVertexArrays(1);
+			auto obj1_refs = obj1->getAllVertexArrays();
+			auto obj2_refs = obj2->getAllVertexArrays();
 
+			//Call destructor of first object
+			obj1->~bVertexArrayObjects();
+
+			//Check the references of the first object.
+			Assert::IsFalse(obj1_refs.at(0).expired());
+			Assert::IsFalse(obj1_refs.at(1).expired());
+			Assert::IsFalse(obj1_refs.at(2).expired());
+			Assert::IsTrue(obj1_refs.at(3).expired()); //The last one should have expired
+
+			//Call destructor of the second object
+			obj2->~bVertexArrayObjects();
+
+			//Check the references of the second object;
+			for each (auto v in obj2_refs)
+			{
+				//All references should be expired
+				Assert::IsTrue(v.expired());
+			}
 		}
 	};
 }
--- Blazer3dviewport/include/bVertexArrayObjects.h
+++ Blazer3dviewport/include/bVertexArrayObjects.h
@@ -28,56 +28,56 @@ private:
 		std::vector<bVertexAttribute> attributes;
 	};
 
-	std::vector<bVertexArray> vertex_arrays;
+	std::vector<std::shared_ptr<bVertexArray>> vertex_arrays;
 
 public:
 	//constructors
 
-	//Default contructor creates only one vertex array object
-	bVertexArrayObjects() {
-		bVertexArray new_vao;
-		new_vao.label = "0";
-		glCreateVertexArrays(1, &new_vao.opengl_name);
-		vertex_arrays.push_back(new_vao);
-	}
+	//Default contructor with empty array of vertex array objects
+	bVertexArrayObjects() { }
 
 	//Create a specified number of vertex array objects.
 	//Label will be the opengl numerical name.
 	bVertexArrayObjects(GLint num_vaos) {
 
 		for (int i = 0; i < num_vaos; i++) {
-			bVertexArray new_vao;
-			new_vao.label = std::to_string(i);
-			glCreateVertexArrays(1, &new_vao.opengl_name);
-			glObjectLabel(GL_VERTEX_ARRAY, new_vao.opengl_name,
-				B_NULL_TERMINATED, new_vao.label.c_str());
-			vertex_arrays.push_back(new_vao);
+			auto ptr = std::make_shared<bVertexArray>(new bVertexArray);
+			glCreateVertexArrays(1, &ptr->opengl_name);
+			ptr->label = std::to_string(ptr->opengl_name);
+			vertex_arrays.push_back(ptr);
 		}
 	}
 
-	//Create a specified number of vertex array objects with labels for each
-	bVertexArrayObjects(GLint num_vaos, const std::vector<std::string>& labels) {
+	//Constructor that takes in an initializer list of labels.
+	//Will create a VAOs for each label.
+	bVertexArrayObjects(const std::initializer_list<std::string> l) {
 
-		for (int i = 0; i < num_vaos; i++) {
-			bVertexArray new_vao;
-			new_vao.label = labels.at(i);
-			glCreateVertexArrays(1, &new_vao.opengl_name);
-			glObjectLabel(GL_VERTEX_ARRAY, new_vao.opengl_name,
-				B_NULL_TERMINATED, labels.at(i).c_str());
-			vertex_arrays.push_back(new_vao);
+		std::initializer_list<std::string>::iterator it;
+		for (it = l.begin(); it != l.end(); it++) {
+			auto ptr = std::make_shared<bVertexArray>(new bVertexArray);
+			ptr->label = *it;
+			glCreateVertexArrays(1, &ptr->opengl_name);
+			glObjectLabel(GL_VERTEX_ARRAY, ptr->opengl_name,
+				B_NULL_TERMINATED, ptr->label.c_str());
+			vertex_arrays.push_back(ptr);
 		}
 	}
 
 	//Destructor
 	//Delete all vertex array objects.
 	~bVertexArrayObjects() {
-		std::vector<bVertexArray>::iterator it;
+		std::vector<std::shared_ptr<bVertexArray>>::iterator it;
 		for (it = vertex_arrays.begin(); it != vertex_arrays.end(); it++) {
-			glDeleteVertexArrays(1, &it->opengl_name);
+			if (it->use_count() == 1)
+			glDeleteVertexArrays(1, &(*it)->opengl_name);
 		}
 	}
 
 	void createVertexAttribute();
+	int size() { return vertex_arrays.size(); }
+	std::shared_ptr<bVertexArray> getVertexArray(const std::string& label);
+	std::shared_ptr<bVertexArray> getVertexArray(const GLuint opengl_name);
+	std::vector<std::weak_ptr<bVertexArray>> getAllVertexArrays();
 
 	GLuint getOpenglName(const std::string& label);
 
