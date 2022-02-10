5497221f9a5c0bf3378c34400da724c3b73d2439
LowFire <LowFire3@yahoo.com>
2022-02-10 08:25:52 -0700

    Commented out bVertexArrayObjects

--- Blazer3dviewport/src/bVertexArrayObjects.cpp
+++ Blazer3dviewport/src/bVertexArrayObjects.cpp
@@ -1,10 +1,10 @@
-#include "bVertexArrayObjects.h"
-
-GLuint bVertexArrayObjects::getOpenglName(const std::string& label) {
-
-	return 0;
-}
-
-void bVertexArrayObjects::createVertexAttribute() {
-
-}
+//#include "bVertexArrayObjects.h"
+//
+//GLuint bVertexArrayObjects::getOpenglName(const std::string& label) {
+//
+//	return 0;
+//}
+//
+//void bVertexArrayObjects::createVertexAttribute() {
+//
+//}
