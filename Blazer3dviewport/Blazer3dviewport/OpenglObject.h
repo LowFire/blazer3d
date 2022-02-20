b8a435f2852061dffdc87c95979e0a3f620e9f7a
LowFire <LowFire3@yahoo.com>
2022-02-20 14:31:04 -0700

    Implemented OpenglObject class methods.

---
+++ Blazer3dviewport/Blazer3dviewport/OpenglObject.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+#include <glad/glad.h>
+
+#include "constants.h"
+
+class OpenglObject {
+private:
+	GLuint m_opengl_name;
+	std::string m_label;
+	GLenum m_object_type;
+
+public:
+	virtual void bind() = 0;
+	virtual void unbind() = 0;
+	virtual void reset() = 0;
+
+	GLuint getOpenglName() { return m_opengl_name; }
+	std::string getLabel() { return m_label; }
+
+	void setLabel(const std::string& label) {
+		this->m_label = label;
+		glObjectLabel(m_object_type,
+			m_opengl_name,
+			B_NULL_TERMINATED,
+			this->m_label.c_str());
+	}
+
+	GLenum getObjectLabel() { return m_object_type; }
+};
