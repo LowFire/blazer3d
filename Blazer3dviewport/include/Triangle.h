0c75eb4ddfb6ccabc308e8e9840f3a6c9e3c2707
LowFire <LowFire3@yahoo.com>
2022-03-09 10:07:26 -0700

    Implemented triangle function stubs.

---
+++ Blazer3dviewport/include/Triangle.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Draw.h"
+
+namespace TestOpenglWrapperAPI
+{
+	class testTriangle;
+}
+
+class Triangle : public Draw
+{
+	friend TestOpenglWrapperAPI::testTriangle;
+private:
+
+public:
+	Triangle();
+	Triangle(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3);
+
+	void draw() override;
+};
