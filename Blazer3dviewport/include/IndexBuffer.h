bc0d5d2fab32b6fbc86dbb4192e3ce55f6dea56b
LowFire <LowFire3@yahoo.com>
2022-03-17 16:20:39 -0600

    Implemented class delaraction.

--- Blazer3dviewport/include/IndexBuffer.h
+++ Blazer3dviewport/include/IndexBuffer.h
@@ -1,8 +1,31 @@
 #pragma once
+
+#include <vector>
+
 #include "Buffer.h"
+
+namespace TestOpenglWrapperAPI
+{
+	class TestIndexBuffer;
+}
 
 class IndexBuffer : public Buffer
 {
+	friend TestOpenglWrapperAPI::TestIndexBuffer;
 private:
+	static GLuint s_currently_bound_ebo;
+	int m_indicies_count;
+
 public:
+	IndexBuffer();
+	IndexBuffer(std::initializer_list<GLuint> l);
+	
+	static void reset();
+	void bind() override;
+	void unbind() override;
+	bool isBound() override;
+
+	void setIndicies(std::initializer_list<GLuint> l);
+	std::vector<GLuint> getIndicies();
+	int getIndiciesCount() { return m_indicies_count; }
 };
